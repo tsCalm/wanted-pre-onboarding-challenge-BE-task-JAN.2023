1b9640eb617bf9545a0fbe1dfac2d62cad3a1752
tsCalm <dlatkdals93@gmail.com>
2023-01-17 18:28:34 +0900

    feat: app.ts

---
+++ lecture-1/app.ts
@@ -0,0 +1,13 @@
+// @ts-ignore
+import express, { Express, NextFunction, Request, Response } from "express";
+import { sqlInjectionPrevention } from "./sql-injection-prevention";
+
+const app: Express = express();
+
+app.get("/test", sqlInjectionPrevention, (req: Request, res: Response) => {
+  res.json(req.query);
+});
+
+app.listen(8000, () => {
+  console.log("server is running");
+});
